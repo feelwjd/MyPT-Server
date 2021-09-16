f982ff855aefdf53369ea29470bd506f680cd4c2
feelwjd <feelwjd@gmail.com>
2021-09-17 00:40:24 +0900

    ?

--- db.js
+++ db.js
@@ -1,8 +1,4 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 const mysql = require('mysql');
-
 const con = mysql.createConnection({
     host: 'ptdata.ceiotvbr944v.ap-northeast-2.rds.amazonaws.com',
     user: 'mypt',
