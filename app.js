d790f1068cf0397f283ee0f74fe99df7b50e25cb
jasonoh22 <jasonoh22@naver.com>
2021-09-16 22:44:33 +0900

    웹 프로젝트 개발

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const port = 8000;
+
+app.get('/', (req, res) =>{
+    res.send('Hello World!')
+})
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+module.exports = app;
