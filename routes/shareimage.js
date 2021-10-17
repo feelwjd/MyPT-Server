d4f5bacd19fda616c3849c36364f0d66d0d1b7ad
feelwjd <feelwjd@gmail.com>
2021-10-18 03:06:56 +0900

    imageshare

--- routes/shareimage.js
+++ routes/shareimage.js
@@ -31,16 +31,26 @@ router.post('/image',upload, function(req, res, next) {
   if(err)
             console.log(err);
   })
+  //fs.readFile('public/shareimage/'+jpgname, function(err,data){
+  //  if(err) throw err;
+  //  res.writeHead(200, {"Context-Type": "image/jpg"});
+  //  res.write(data);
+  //  res.end();
+  //})
+  //res.setHeader('Content-Disposition', `attachment; filename = ${jpgname}`);
+  //res.writeHead(200, {"Context-Type": "image/jpg"})
+  //res.download('public/shareimage/',jpgname);
+  //res.end(jpgname);
+  res.render('/imageshare',{jpgname:'jpgname'});
+});
+
+router.post('/imageshare',function(req,res,next){
+  jpgname = req.body.jpgname;
   fs.readFile('public/shareimage/'+jpgname, function(err,data){
     if(err) throw err;
     res.writeHead(200, {"Context-Type": "image/jpg"});
     res.write(data);
     res.end();
-  })
-  //res.setHeader('Content-Disposition', `attachment; filename = ${jpgname}`);
-  //res.writeHead(200, {"Context-Type": "image/jpg"})
-  //res.download('public/shareimage/',jpgname);
-  //res.end(jpgname);
 });
 
 module.exports = router;
