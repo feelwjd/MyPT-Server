cde492099cbc37361b490d8fb34963f2b6587bed
feelwjd <feelwjd@gmail.com>
2021-10-18 03:02:25 +0900

    shareimage

--- routes/shareimage.js
+++ routes/shareimage.js
@@ -22,7 +22,7 @@ var resizeX = 1080
 /* GET home page. */
 router.post('/image',upload, function(req, res, next) {
   var jpgname = req.file.fieldname + '-'+ Date.now()+'.jpg';
- gm(req.file.path)
+  gm(req.file.path)
     .resize(resizeX, resizeY)
     .fill('#ffffff')
     .font('public/font/BMJUA_ttf.ttf', 30)
@@ -31,10 +31,16 @@ router.post('/image',upload, function(req, res, next) {
   if(err)
             console.log(err);
   })
-  res.setHeader('Content-Disposition', `attachment; filename = ${jpgname}`);
-  res.writeHead(200, {"Context-Type": "image/jpg"})
+  fs.readFile('public/shareimage/'+jpgname, function(err,data){
+    if(err) throw err;
+    res.writeHead(200, {"Context-Type": "image/jpg"});
+    res.write(data);
+    res.end();
+  })
+  //res.setHeader('Content-Disposition', `attachment; filename = ${jpgname}`);
+  //res.writeHead(200, {"Context-Type": "image/jpg"})
   //res.download('public/shareimage/',jpgname);
-  res.end(jpgname);
+  //res.end(jpgname);
 });
 
 module.exports = router;
