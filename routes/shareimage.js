cefaa770096ca47c7a74c5506cae3408c5cafe9d
feelwjd <feelwjd@gmail.com>
2021-10-22 20:58:51 +0900

    imageshare update

--- routes/shareimage.js
+++ routes/shareimage.js
@@ -58,7 +58,7 @@ router.post('/image',upload, function(req, res, next) {
         });
       }
     })
-  
+  console.log(workouts);
   //res.setHeader('Content-Disposition', `attachment; filename = ${jpgname}`);
   //res.writeHead(200, {"Context-Type": "image/jpg"})
   //res.download('public/shareimage/',jpgname);
