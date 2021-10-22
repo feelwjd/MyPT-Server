9d98c05f4206ace0f5c0d3a1f5abaa232d41cf1b
feelwjd <feelwjd@gmail.com>
2021-10-22 21:21:26 +0900

    imageshare update

--- routes/shareimage.js
+++ routes/shareimage.js
@@ -37,27 +37,28 @@ router.post('/image',upload, function(req, res, next) {
     if (err) throw err;
     else{
       for(var idx in results){
-        con.query("select * from workout where workoutid in ('"+results[idx].workoutid+"')",function(err,results){
-          workouts.push(results.workoutname);
-        });
+        workouts.push(results[idx].workoutid);
       }
-      gm(req.file.path)
-      .resize(resizeX, resizeY)
-      .fill('#ffffff')
-      .font('public/font/BMJUA_ttf.ttf', 50).drawText(75,75,"MyPT")   // 폰트 설정// 텍스트 주입
-      .font('public/font/BMJUA_ttf.ttf', 40).drawText(75,800,"오늘의 루틴")               
-      .font('public/font/BMJUA_ttf.ttf', 30).drawText(75,900,""+workouts+"")
-      .write('public/shareimage/'+jpgname ,function(err){
-        if(err){console.log(err);}  
-        else{
-          fs.readFile('public/shareimage/'+jpgname, function(err,data){   // 편집한 이미지 반환하여 보여주기
-            if(err) throw err;
-            res.writeHead(200, {"Context-Type": "image/jpg"});
-            res.write(data);
-            res.end();
-          });
-        }
-      })
+      con.query("select * from workout where workoutid in ('"+workouts+"')",function(err,results){
+        gm(req.file.path)
+        .resize(resizeX, resizeY)
+        .fill('#ffffff')
+        .font('public/font/BMJUA_ttf.ttf', 50).drawText(75,75,"MyPT")   // 폰트 설정// 텍스트 주입
+        .font('public/font/BMJUA_ttf.ttf', 40).drawText(75,800,"오늘의 루틴")               
+        .font('public/font/BMJUA_ttf.ttf', 30).drawText(75,900,""+results.workoutname+"")
+        .write('public/shareimage/'+jpgname ,function(err){
+          if(err){console.log(err);}  
+          else{
+            fs.readFile('public/shareimage/'+jpgname, function(err,data){   // 편집한 이미지 반환하여 보여주기
+              if(err) throw err;
+              res.writeHead(200, {"Context-Type": "image/jpg"});
+              res.write(data);
+              res.end();
+            });
+          }
+        })
+      });
+      
     }
   });
   
