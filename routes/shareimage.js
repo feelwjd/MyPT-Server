f95b8e5649eb8bf2a90924770f61716e3ed45ffe
feelwjd <feelwjd@gmail.com>
2021-10-22 21:08:40 +0900

    imageshare update

--- routes/shareimage.js
+++ routes/shareimage.js
@@ -39,26 +39,26 @@ router.post('/image',upload, function(req, res, next) {
       for(var idx in results){
         workouts.push(results[idx].workoutid);
       }
-      console.log(workouts);
+      gm(req.file.path)
+      .resize(resizeX, resizeY)
+      .fill('#ffffff')
+      .font('public/font/BMJUA_ttf.ttf', 50).drawText(75,75,"MyPT")   // 폰트 설정// 텍스트 주입
+      .font('public/font/BMJUA_ttf.ttf', 40).drawText(75,800,"오늘의 루틴")               
+      .font('public/font/BMJUA_ttf.ttf', 30).drawText(75,900,""+workouts+"")
+      .write('public/shareimage/'+jpgname ,function(err){
+        if(err){console.log(err);}  
+        else{
+          fs.readFile('public/shareimage/'+jpgname, function(err,data){   // 편집한 이미지 반환하여 보여주기
+            if(err) throw err;
+            res.writeHead(200, {"Context-Type": "image/jpg"});
+            res.write(data);
+            res.end();
+          });
+        }
+      })
     }
   });
-  gm(req.file.path)
-    .resize(resizeX, resizeY)
-    .fill('#ffffff')
-    .font('public/font/BMJUA_ttf.ttf', 50).drawText(75,75,"MyPT")   // 폰트 설정// 텍스트 주입
-    .font('public/font/BMJUA_ttf.ttf', 40).drawText(75,800,"오늘의 루틴")               
-    .font('public/font/BMJUA_ttf.ttf', 30).drawText(75,900,""+workouts+"")
-    .write('public/shareimage/'+jpgname ,function(err){
-      if(err){console.log(err);}  
-      else{
-        fs.readFile('public/shareimage/'+jpgname, function(err,data){   // 편집한 이미지 반환하여 보여주기
-          if(err) throw err;
-          res.writeHead(200, {"Context-Type": "image/jpg"});
-          res.write(data);
-          res.end();
-        });
-      }
-    })
+  
   console.log(workouts);
   //res.setHeader('Content-Disposition', `attachment; filename = ${jpgname}`);
   //res.writeHead(200, {"Context-Type": "image/jpg"})
