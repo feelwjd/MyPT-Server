947063d1919b4a21f5682a9bf94a2820f368f950
jasonoh22 <jasonoh22@naver.com>
2021-10-24 15:54:40 +0900

    루틴 수정 및 비포에프터수정

--- routes/func.js
+++ routes/func.js
@@ -2,8 +2,19 @@ var express = require('express');
 const app = require('../app');
 var router = express.Router();
 const mysql = require('mysql');
+const multer = require("multer");
+const path = require("path");
 
-
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/images/");
+  },
+  function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    cb(null, path.basename(file.originalname, ext) + "-" + Date.now() + ext);
+  },
+});
+var upload = multer({ storage: storage });
 router.use(function(req, res, next){
     next();
 });
@@ -20,12 +31,26 @@ router.post('/', function(req, res, next) {
     res.status(201).json('"messeage" : "success"'); 
   });
 
+router.post('/calories_cal', function(req, res, next) {
+    var count = req.body.count; // 1분 몇개인지
+    var weight = req.body.weight;
+    var userid = req.body.userid;
+    var today = new Date();
+    var day = now.getData();
+    var total_cal = ((21 * weight)*0.0005) * count; // 1분운동한 칼로리
+    let sql = 'INSERT INTO calories, day VALUES where userid=?'
+    let params = [userid];
+    con.query(sql , params, function(err, result){
+        res.status(201).json('"messeage" : "success"'); 
+    })
+    
+});
 router.post('/mkroutine', function(req, res, next){
     res.status(201).json('"messeage" : "success"');
 });
 
-router.post('/picshare', function(req, res, next){
-
-    res.status(201).json('"messeage" : "success"');
+router.post('/picshare',upload.single('image'), (req, res)=>{
+    let image = req.file.path;
+    res.status(201).json(image);
 });
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -26,32 +26,49 @@ const con = mysql.createConnection({
 	host: 'ptdata.ceiotvbr944v.ap-northeast-2.rds.amazonaws.com',
 	user: 'mypt',
 	password: '12345678',
-	database: 'mypt'
+	database: 'mypt',
+  multipleStatements: true
 });
 
+con.connect();
 /* GET home page. */
 router.post('/', function(req, res, next) {
     res.status(201).json('"messeage" : "success"'); 
   });
 //회원가입
 router.post('/signup', upload.single("image"), function(req, res, next){  
-    user_id = req.body.userid;
-    password = req.body.pw;
-    username = req.body.username;
-    height = req.body.height;
-    weight = req.body.weight;
-    sex = req.body.sex;
-    const image = `/images/${req.file.filename}`;
-    var sql = "select userid from users where userid=?";
-    var id = [user_id];
-    con.query(sql, id, function(err, result){
-        res.status(201).json('"messeage" : "id exist"'); 
-    })
-    con.query("insert into users(userid, pw, username, height, weight, sex) values ('"+user_id+"','"+password+"','"+username+"','"+height+"','"+weight+"','"+sex+"','"+image+"')"
-    , function(err, result){
-      if (err) throw res.json(err);
-      res.json('success');
-    })    
+  user_id = req.body.userid;
+  password = req.body.pw;
+  username = req.body.username;
+  height = req.body.height;
+  weight = req.body.weight;
+  sex = req.body.sex;
+  image = req.body.image;
+  var sql = "select userid from users where userid=?;";
+  var id = [user_id];
+  var sql1 = mysql.format(sql, id);
+  var sql2 = "insert into users(userid, pw, username, height, weight, sex, image) values ('"+user_id+"','"+password+"','"+username+"','"+height+"','"+weight+"','"+sex+"','"+image+"');"
+  var sql3 = "insert into UserBeforeAfter(userid, before_pic, weight, height) values('"+user_id+"','"+image+"','"+weight+"','"+height+"');"
+
+  con.query(sql1+sql2, function(err, result){
+      con.query(sql3, function(err, result1){
+        if(err){
+          console.log(err);
+        }
+      })
+      console.log(111);    
+  })
+  // con.query("insert into users(userid, pw, username, height, weight, sex, image) values ('"+user_id+"','"+password+"','"+username+"','"+height+"','"+weight+"','"+sex+"','"+image+"')"
+  // , function(err, result){
+  //   if (err) throw res.json(err);   
+  //   res.json('sucesss');
+  // })   
+ 
+  // con.query("insert into UserBeforeAfter(userid, before, weight, height) values('"+user_id+"','"+image+"','"+height+"','"+weight+"')"
+  // , function(err, result){
+  //   if (err) throw res.json(err);
+  // }) 
+  // res.json('sucesss');
 });
 //로그인
 router.post('/signin', function(req, res, next){  
