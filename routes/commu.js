56e681b02906dcf935d97853d785ce5265f5ffa2
jasonoh22 <jasonoh22@naver.com>
2021-10-25 21:25:03 +0900

    커뮤니티

--- routes/commu.js
+++ routes/commu.js
@@ -20,11 +20,49 @@ router.post('/', function(req, res, next) {
   });
 // 공유
 router.post('/share', function(req, res, next){   
-    res.status(201).json('"messeage" : "success"'); 
+    
+    var user_id = req.body.userid;
+    var commudescript = req.body.commudescript;
+    var sql = "select heart from community where userid=?"
+    con.query(sql, [user_id], function(err,result){
+        if(err){
+            console.log(err)
+        }
+        var heart = result[0].heart
+        var sql1 = "select image from community where userid=?"
+        con.query(sql1, [user_id], function(err, result1){
+            if(err){
+                console.log(err)
+            }
+            console.log("성공");
+            var image = result1[0].image;
+
+            data = {commudescript, image, heart}
+            res.json(data);
+        })
+    })
 });
 //좋아요
-router.post('/i_like_you', function(req, res, next){   
-    res.status(201).json('"messeage" : "success"'); 
+router.put('/heart', function(req, res, next){   
+    var user_id = req.body.userid;
+    var heart = req.body.heart;
+    //boolean 필요
+    var check = req.body.check;
+    var count;
+    if(check == 0){
+        count = heart -1;
+    }
+    else{
+        count = heart + 1
+    }
+    var sql = "Update community set heart=? where userid=?"
+    con.query(sql,[count, user_id], function(err, result){
+        if(err){
+            console.log(err);
+        }
+        res.status(201).json('"messeage" : "success"'); 
+    })
+    
 });
 
 module.exports = router;
