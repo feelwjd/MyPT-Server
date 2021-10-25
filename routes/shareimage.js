4b9e77fc4f415c7cade9ba891695db48b4d54fae
feelwjd <feelwjd@gmail.com>
2021-10-25 20:07:52 +0900

    share image

--- routes/shareimage.js
+++ routes/shareimage.js
@@ -59,7 +59,7 @@ router.post('/image',upload, function(req, res, next) {
               res.writeHead(200, {"Context-Type": "image/jpg"});
               res.write(data);
               res.end();
-              con.query("insert into commuity(userid, imagename) values('"+userid+"','"+jpgname+"')",function(err,results){
+              con.query("insert into community(userid, imagename) values('"+userid+"','"+jpgname+"')",function(err,results){
                 if (err) throw err;
               });
             });
