3b0c0ec5e5372642b013cd02b74c5cae9ca83427
feelwjd <feelwjd@gmail.com>
2021-11-01 16:32:15 +0900

    share image

--- routes/shareimage.js
+++ routes/shareimage.js
@@ -49,15 +49,15 @@ router.post('/image',upload, function(req, res, next) {
         gm(req.file.path)
         .resize(resizeX, resizeY)
         .fill('#ffffff')
-        .font('public/font/BMJUA_ttf.ttf', 50).drawText(75,75,"MyPT")   // 폰트 설정// 텍스트 주입
-        .font('public/font/BMJUA_ttf.ttf', 40).drawText(75,900,"오늘의 루틴")               
-        .font('public/font/BMJUA_ttf.ttf', 25).drawText(75,1000,""+workoutlist+"")
+        .font('public/font/BMJUA_ttf.ttf', 50).drawText(105,105,"MyPT")   // 폰트 설정// 텍스트 주입
+        .font('public/font/BMJUA_ttf.ttf', 40).drawText(105,750,"오늘의 루틴")               
+        .font('public/font/BMJUA_ttf.ttf', 25).drawText(105,850,""+workoutlist+"")
         .write('public/shareimage/'+jpgname ,function(err){
           if(err){console.log(err);}  
           else{
             fs.readFile('public/shareimage/'+jpgname, function(err,data){   // 편집한 이미지 반환하여 보여주기
               if(err) throw err;
-              res.writeHead(200, {"Context-Type": "image/png"});
+              res.writeHead(200, {"Context-Type": "image/jpg"});
               res.write(data);
               res.end();
               con.query("insert into community(userid, image, commudescript) values('"+userid+"','"+jpgname+"','"+descript+"')",function(err,results){
